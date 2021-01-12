00347d77e4e97171f8ee282188652506223f469a
salomao270 <salomao270@hotmail.com>
2021-01-12 00:02:23 -0300

    release project files

---
+++ Wcf_Example/EmailService.svc.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Wcf_Example
+{
+    // NOTE: In order to launch WCF Test Client for testing this service, select IEmailService.svc at the Solution Explorer and run it.
+    public class EmailBO : IEmailService
+    {
+        public bool ValidateEmail(string email)
+        {
+            string isValidEmail = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            return Regex.IsMatch(email, isValidEmail);
+        }
+    }
+}
---
+++ Wcf_Example/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace Wcf_Example
+{
+    [ServiceContract]
+    public interface IEmailService
+    {
+        [OperationContract]
+        bool ValidateEmail(string email);
+    }
+
+}
